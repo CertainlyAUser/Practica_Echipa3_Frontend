8b970a4c6a83cb7576cf2cab7935e202d9c50060
CertainlyAUser <sabin.harsan@gmail.com>
2020-12-15 19:27:30 +0200

    Added error messages

--- src/app/core/editannouncement/editannouncement.component.ts
+++ src/app/core/editannouncement/editannouncement.component.ts
@@ -1,3 +1,4 @@
+import { STEPPER_GLOBAL_OPTIONS } from '@angular/cdk/stepper';
 import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators, FormControl} from '@angular/forms'
 import { ActivatedRoute } from '@angular/router';
@@ -7,7 +8,10 @@ import { TagService } from 'src/app/services/tag.service';
 @Component({
   selector: 'app-editannouncement',
   templateUrl: './editannouncement.component.html',
-  styleUrls: ['./editannouncement.component.scss']
+  styleUrls: ['./editannouncement.component.scss'],
+  providers: [{
+    provide: STEPPER_GLOBAL_OPTIONS, useValue: {showError: true}
+  }]
 })
 export class EditannouncementComponent implements OnInit {
 
@@ -38,6 +42,7 @@ export class EditannouncementComponent implements OnInit {
       this.timeInfo.controls.date.setValue(x.date);
       this.timeInfo.controls.location.setValue(x.location);
       this.miscInfo.controls.requirements.setValue(x.requirements)
+      x.tags.forEach(y => ts.addTag(y.text.toString()));
     })
     this.showModal = false;
     this.generalInfo = this.fb.group({
@@ -82,4 +87,33 @@ export class EditannouncementComponent implements OnInit {
   ngOnInit() {
   }
 
+  toggleShow(){
+    this.showModal = !this.showModal;  
+  }
+
+  onSubmit(){
+    this.ts.addTag(this.tagInfo.controls.tags.value);
+    this.tagInfo.reset();
+  }
+
+  createAnnouncement(){
+    if( this.generalInfo.valid && this.descripInfo.valid && this.timeInfo.valid && this.miscInfo.valid){
+      this.announcement.title = this.generalInfo.controls.title.value;
+      this.announcement.link = this.generalInfo.controls.link.value;
+      this.announcement.type = this.generalInfo.controls.type.value;
+      this.announcement.vacantPositions = this.generalInfo.controls.vacantPositions.value;
+      this.announcement.price = this.generalInfo.controls.price.value;
+      this.announcement.shortDesc = this.descripInfo.controls.shortDesc.value;
+      this.announcement.description = this.descripInfo.controls.description.value;
+      this.announcement.startDate = this.timeInfo.controls.startDate.value;
+      this.announcement.limitDate = this.timeInfo.controls.limitDate.value;
+      this.announcement.date = this.timeInfo.controls.date.value;
+      this.announcement.location = this.timeInfo.controls.location.value;
+      this.announcement.prize = this.miscInfo.controls.prize.value;
+      this.announcement.requirements = this.miscInfo.controls.requirements.value;
+      this.announcement.tags = this.ts.getTags();
+      this.ts.clear();
+    }
+    console.log(this.announcement);
+  }
 }
