978250767b954cc1b956c2b359f924fac9ffa4d5
Razvan Neta <razvanneta@gmail.com>
2020-12-15 16:34:36 +0200

    Added login componenet and routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -8,7 +8,7 @@ import { RegisterComponent } from './register';
 import { AuthGuard } from './_helpers';
 
 const routes: Routes = [
-  { path: '', component: HomeComponent, canActivate: [AuthGuard] },
+  { path: '', component: LandingPageComponent },
   { path: 'home', component: HomeComponent },
   { path: 'login', component: LoginComponent },
   { path: 'register', component: RegisterComponent },
--- src/app/landing-page/landing-page.component.ts
+++ src/app/landing-page/landing-page.component.ts
@@ -1,9 +1,17 @@
 import { Component, OnInit } from '@angular/core';
+import {User} from '../_user_model';
+import {AuthenticationService} from '../_services';
 
 @Component({ templateUrl: 'landing-page.component.html' })
 export class LandingPageComponent implements OnInit {
 
-    constructor() {}
+    currentUser: User;
+
+
+
+    constructor(private authService: AuthenticationService) {
+        authService.currentUser.subscribe(x => this.currentUser = x);
+    }
 
     ngOnInit() {
     }
